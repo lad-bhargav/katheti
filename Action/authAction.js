78077853c83f1e0470536406dd8774207b4af65d
lad-bhargav <ladshiv867@gmail.com>
2025-10-15 13:25:54 +0530

    second commit

---
+++ Action/authAction.js
@@ -0,0 +1,23 @@
+'use server'
+import { connectDB } from "@/Utils/mongooseconnect";
+import Auth from "@/Schema/authSchema.js";
+import { parseJSON } from "@/Utils/parse";
+
+//signup
+
+export const signupAction = async(user) => {
+    try{
+        await connectDB();
+        console.log("connected with DB")
+        const userTest = await Auth.create({
+      name: "sam",
+      email: "sam@example.com",
+      password: "123456",
+    });
+    console.log("user is added")
+        //const newUser = await Auth.create(user);
+        return {"msg" : "signupDone"}
+    }catch(err){
+       throw new Error("Internal server error");
+    }
+}
---
+++ Schema/authSchema.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+const AuthSchema = new mongoose.Schema({
+    username : {type : String},
+    email : {type : String},
+    password : {type : String},
+    image : {type : String},
+});
+
+export default mongoose.models.Auth || mongoose.model("Auth",AuthSchema);
